a6f6611fbac5a3aa4fb8cc4a5e8eb42ff39198bf
acenturion <armandocenturionsa@gmail.com>
2020-07-18 16:15:19 -0400

    Mensaje de Confirmacion

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -4,6 +4,7 @@ jQuery(document).ready(function($) {
   //Contact
   $('form.contactForm').submit(function() {
     var f = $(this).find('.form-group'),
+
       ferror = false,
       emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
 
@@ -11,7 +12,7 @@ jQuery(document).ready(function($) {
 
       var i = $(this); // current input
       var rule = i.attr('data-rule');
-
+      //hola mundo
       if (rule !== undefined) {
         var ierror = false; // error flag for current input
         var pos = rule.indexOf(':', 0);
